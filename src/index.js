a04bbeed6bf1a7e2e14dd19a5d61bce2900abca5
Lena <summyalena@gmail.com>
2022-09-20 21:03:10 +0100

    made updates to the index.js file

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import store from './redux/configureStore';
+import './index.css';
 import App from './App';
 import Category from './category';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/category" element={<Category />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/category" element={<Category />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
