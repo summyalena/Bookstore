0dbe3a77ef4042b5d692dae080860dff9e67e123
Lena <summyalena@gmail.com>
2022-09-20 21:01:51 +0100

    made updates to addition of books in redux and ui

--- src/components/form.js
+++ src/components/form.js
@@ -1,19 +1,40 @@
 /*eslint-disable */
-
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { actions } from '../redux/books/books';
+import {v4 as uuidv4} from 'uuid'
 import './form.css';
 
-
 function form() {
- 
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+
+  const handleChangeTitle = (e) => {
+      setTitle(e.target.value)
+  };
+
+  const handleChangeAuthor = (e) => {
+     setAuthor(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (title.trim() && author.trim()) {
+      dispatch(actions.ADDBOOK({id: uuidv4(), title, author}));
+      setTitle('');
+      setAuthor('')
+    } else {
+      alert('input an author/title');
+    }
+  };
   return (
-    <form className="form">
+    <form className="form" onSubmit={handleSubmit}>
       <h4>ADD NEW BOOK</h4>
       <div className="input">
-        <input value='' type="text" placeholder="Book title" />
-        <select className="select" id="author" name="author">
-          <option value="author">author</option>
-        </select>
+        <input onChange={handleChangeTitle} type="text" value={title} name="title" placeholder="Book title" />
+        <input onChange={handleChangeAuthor} id="author" value={author} type="text" placeholder='Book author' name="author" />
         <button type="submit">ADD BOOK</button>
       </div>
     </form>
