f5f62864cb30e710d9313355ee9b5afc4212b04b
Lena <summyalena@gmail.com>
2022-09-23 12:52:24 +0100

    updated form.js

--- src/components/form.js
+++ src/components/form.js
@@ -4,12 +4,13 @@ import { useDispatch } from 'react-redux';
 import { actions } from '../redux/books/books';
 import {v4 as uuidv4} from 'uuid'
 import './form.css';
+import {postBooks} from '../redux/books/books'
 
 function form() {
   const dispatch = useDispatch();
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
-
+  const [category, setCategory] = useState('');
 
   const handleChangeTitle = (e) => {
       setTitle(e.target.value)
@@ -19,12 +20,18 @@ function form() {
      setAuthor(e.target.value);
   }
 
+  const handleCategoryHandler = (e)=> {
+      setCategory(e.target.value);
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (title.trim() && author.trim()) {
-      dispatch(actions.ADDBOOK({id: uuidv4(), title, author}));
+      // dispatch(actions.ADDBOOK({id: uuidv4(), title, author}));
+       dispatch(postBooks({item_id: uuidv4(), author, title, category}))
       setTitle('');
       setAuthor('')
+      setCategory('')
     } else {
       alert('input an author/title');
     }
@@ -35,6 +42,7 @@ function form() {
       <div className="input">
         <input onChange={handleChangeTitle} type="text" value={title} name="title" placeholder="Book title" />
         <input onChange={handleChangeAuthor} id="author" value={author} type="text" placeholder='Book author' name="author" />
+        <input onChange={handleCategoryHandler} id="category" value={category} type="text" placeholder='Book Category' name="category" />
         <button type="submit">ADD BOOK</button>
       </div>
     </form>
