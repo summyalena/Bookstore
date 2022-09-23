0b62c175920a05418e8b48502cc645997ac1d8f2
Lena <summyalena@gmail.com>
2022-09-23 12:53:36 +0100

    made updates on BooksDisplay file

--- src/components/BooksDisplay.js
+++ src/components/BooksDisplay.js
@@ -1,16 +1,27 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Book from './Book';
 import Form from './form';
+import { fetchBooks } from '../redux/books/books';
 
 function BooksDisplay() {
-  const booksCategory = useSelector((state) => state.books);
+  const booksCategory = useSelector((state) => state.books.books);
+  // const loading = useSelector((state) => state.books.loading);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchBooks());
+    // console.log('fellow');
+  }, [dispatch]);
+
+  // if (loading) return <p>Loading...</p>;
+
   const books = booksCategory.map((book) => (
     <Book id={book.id} key={book.id} author={book.author} title={book.title} />
   ));
   return (
     <div className="container">
-      {books}
+      { books }
       <Form />
     </div>
   );
