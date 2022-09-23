5286d510c2df106dab708d438b4d3dfb99851cbd
Lena <summyalena@gmail.com>
2022-09-23 12:54:26 +0100

    made updates on books.js

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,28 +1,68 @@
 /* eslint-disable no-param-reassign */
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-// const initialState = [
-//   {
-//     id: 1,
-//     title: 'life',
-//     author: 'okolike',
-//   },
-//   {
-//     id: 2,
-//     title: 'life',
-//     author: 'okolike',
-//   },
-// ];
+const API = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/pgaZX3KccKEE6Meztdoy/books';
+
+export const fetchBooks = createAsyncThunk('fetch-books', async () => {
+  const response = await axios.get(API);
+  // console.log(response);
+  const result = await response.data;
+  const book = Object.entries(result).map((res) => ({
+    id: res[0],
+    ...res[1][0],
+  }));
+  return book;
+});
+
+export const postBooks = createAsyncThunk('post-data', async (data) => {
+  const response = await fetch(API, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'content-Type': 'application/json',
+    },
+  }).then((data) => data.json);
+  return response;
+});
+
+export const removeBook = createAsyncThunk('delete/REMOVEBOOK', async (id) => {
+  await fetch(`${API}/${id}`, {
+    method: 'DELETE',
+  });
+  return id;
+});
+
+const initialState = {
+  books: [],
+  loading: false,
+};
+
 const bookSlice = createSlice({
   name: 'books',
-  initialState: [],
+  initialState,
   reducers: {
     ADDBOOK(state, action) {
-      return ([...state, action.payload]);
+      return ([...state.books, action.payload]);
     },
     REMOVEBOOK(state, action) {
-      return state.filter((book) => book.id !== action.payload.id);
+      return state.books.filter((book) => book.id !== action.payload.id);
     },
+  },
+  extraReducers: {
+    [fetchBooks.pending]: (state) => {
+      state.loading = true;
+    },
+    [fetchBooks.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.books = action.payload;
+    },
+    [fetchBooks.rejected]: (state) => {
+      state.loading = false;
+    },
+    // [postBooks.fulfilled]: (state, action) => {
+    //   state.books = [...state.books, action.payload];
+    // },
   },
 });
 
